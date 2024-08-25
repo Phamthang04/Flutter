ca6e8aedea5f382a2a28c05e86d4faa8814b5f90
Phamthang04 <phamthang040402@gmail.com>
2024-08-25 10:53:36 +0700

    test

---
+++ bai1.dart
@@ -0,0 +1,29 @@
+// // voice main () {
+// //     String s = 'Hello world'
+// //     String s1 = "Hello
+// // }
+
+// Future<void> main(List<String> args) async {
+//     String s= 'hello World';
+//   String s1= "hello World";
+//   List<String> splittedStrs = s.split("");
+//   print("splittedStrs -> $splittedStrs"); 
+//    bool hasWorldStr = s.contains("world");
+//      String sUpperCase = s.toUpperCase();
+//      String sLowerCase = s.toLowerCase();
+//       print("sUpperCase -> $sUpperCase");
+//       print("sUpperCase -> $sUpperCase"); 
+//       int sIndexOf = s.indexOf("o");
+//   print("sIndexOf -> $sIndexOf");
+//   print("sLowerCase -> $sLowerCase"); 
+//   //thay thế các ký tự trong 1 chuỗi
+//    String sReplaced = s.replaceAll("world", "everyone");
+//   print("sReplaced -> $sReplaced");
+//   //loại bỏ khoảng trắng ở đầu cuối String
+//   print("s -> \"$s\"");//sẽ in ra ==> "hello world"
+//   print('s ->"$s"'); //sẽ in ra $ ==> 'Hello uword"
+  
+// }
+
+
+
---
+++ bai2.dart
@@ -0,0 +1,31 @@
+// // void main(List<String> args) {
+// //   double a = 10;
+// //   double b = -10.0;
+
+// //   int c = 10;
+// //   int d = -10;
+  
+// //   int _resultInt = (a + b + c +d).toInt();
+// //   double _resultDouble = (a + b + c +d);
+
+// //  print("_resultInt -> $_resultInt");
+// //  print("_resultDouble -> $_resultDouble");
+// // String _aStr = a.toString();
+// // String _bStr = b. toString();
+// // print("int a toString: $_aStr");
+// // print("double b toString:$_bStr");
+
+// // }
+// void main(List<String> args) {
+//   bool a = true;
+//   bool b = false;
+  
+
+// }
+void main(List<String> args) {
+  Map map = {"key": "value"};
+  Map<String, String> map1 = Map<String, String>();
+  map1["key"] = "value";
+
+
+}
