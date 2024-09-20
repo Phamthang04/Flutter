086a9188eeb8a049c14639f02fa75a7af2ab6cbc
Phamthang04 <94545485+Phamthang04@users.noreply.github.com>
2024-09-20 22:23:52 +0700

    Add files via upload

---
+++ baitap/API.dart
@@ -0,0 +1,32 @@
+{
+    "time": {
+        "updated": "Sep 20, 2024 06:05:00 UTC",
+        "updatedISO": "2024-09-20T06:05:00+00:00",
+        "updateduk": "Sep 20, 2024 at 07:05 BST"
+    },
+    "disclaimer": "This data was produced from the CoinDesk Bitcoin Price Index (USD & GBP) API at https://www.coindesk.com/price/bitcoin.",
+    "chartName": "Bitcoin",
+    "bpi": {
+        "USD": {
+            "code": "USD",
+            "symbol": "&#36;",
+            "rate": "27,564.3500",
+            "description": "United States Dollar",
+            "rate_float": 27564.35
+        },
+        "GBP": {
+            "code": "GBP",
+            "symbol": "&pound;",
+            "rate": "21,342.5114",
+            "description": "British Pound Sterling",
+            "rate_float": 21342.5114
+        },
+        "EUR": {
+            "code": "EUR",
+            "symbol": "&euro;",
+            "rate": "25,682.1014",
+            "description": "Euro",
+            "rate_float": 25682.1014
+        }
+    }
+}
